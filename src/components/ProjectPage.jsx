589526301474da3390d49f6e31f6f57ded8a8e05
HimanshuBisht23L <himanshiubisht4532@gmail.com>
2025-04-18 13:43:44 +0530

    DFS ADDED

--- src/components/ProjectPage.jsx
+++ src/components/ProjectPage.jsx
@@ -4,24 +4,30 @@ import '../styles/Projectpage.css'
 export default function ProjectPage() {
 
     const canva = useRef(null)
+    const AddOutput = useRef(null);
+    const [whichOut, setWhichOut] = useState(false)
     const [startAddVtx, stopAddVtx] = useState(false)
     const [Vertix_data, updateWord] = useState("Add Vertex")
+    const [startVal, setStartVal] = useState("")
 
     const [addedge, setaddedge] = useState(false)
     const [edges, setedges] = useState([]);
     const [selectedVertix, setSelectedVertix] = useState([]) 
 
     const [circleArray, setCircleArray] = useState([])
+    const [Adjlist, setAdjList] = useState({})
 
-    function Circle(x, y, radius) {
+    function Circle(id, x, y, radius, color) {
+        this.color = color
+        this.id = id
         this.x = x
         this.y = y
         this.radius = radius
 
-        this.draw = (ptr, i) => {
+        this.draw = (ptr) => {
             ptr.beginPath();
             ptr.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-            ptr.fillStyle = "black"
+            ptr.fillStyle = this.color
             ptr.fill()
             ptr.stroke()
 
@@ -29,7 +35,7 @@ export default function ProjectPage() {
             ptr.font = "20px Arial";
             ptr.textAlign = "center";
             ptr.textBaseline = "middle";
-            ptr.fillText(i, this.x, this.y);
+            ptr.fillText(this.id, this.x, this.y);
         }
     }
 
@@ -56,8 +62,8 @@ export default function ProjectPage() {
         ptr.clearRect(0, 0, cnva.width, cnva.height);
 
         edges.forEach(edge => edge.draw(ptr));
-        let i = 1;
-        circleArray.forEach(circle => circle.draw(ptr, i++));
+        // let i = 0;
+        circleArray.forEach(circle => circle.draw(ptr));
     }
 
     const getCircle = (e) => {
@@ -83,10 +89,28 @@ export default function ProjectPage() {
                     const Newedge = [...edges, newline]
                     setedges(Newedge);
                     setSelectedVertix([]);
+
+
+                    // Add Element to Adjlist
+                    let a = selection[0].id
+                    let b = selection[1].id
+
+                    function adjlistadd(prev, a, b) {
+                        const updatedAdjlist = {...prev}
+                        if(!updatedAdjlist[a]) updatedAdjlist[a] = []
+                        if(!updatedAdjlist[b]) updatedAdjlist[b] = []
+                        updatedAdjlist[a].push(b) 
+                        updatedAdjlist[b].push(a)
+                        return updatedAdjlist;
+                    }
+                    setAdjList(prev => adjlistadd(prev, a, b))
                 }
             }
         } else if (startAddVtx) {
-            let circle = new Circle(xpos, ypos, 25)
+            //This is the value of Nodes
+            let id = circleArray.length
+            let color = "black"
+            let circle = new Circle(id, xpos, ypos, 25, color)
             const newCircle = [...circleArray, circle]
             setCircleArray(newCircle)
         }
@@ -96,14 +120,6 @@ export default function ProjectPage() {
     useEffect(() => {
         DrawCanvas();
     }, [circleArray, edges]);
-
-    const clearTheCanva = () => {
-        setCircleArray([])
-        setedges([])
-        const cnva = canva.current;
-        let ptr = cnva.getContext("2d");
-        ptr.clearRect(0, 0, cnva.width, cnva.height);
-    };
 
     const Make_Vertex = () => {
         if (addedge === true) {
@@ -134,7 +150,82 @@ export default function ProjectPage() {
         }
     }
 
-    
+
+    const clearTheCanva = () => {
+        setaddedge(false)
+        stopAddVtx(false);
+        setCircleArray([])
+        setedges([])
+        setWhichOut(false)
+        AddOutput.current.innerHTML = "";
+        const cnva = canva.current;
+        let ptr = cnva.getContext("2d");
+        ptr.clearRect(0, 0, cnva.width, cnva.height);
+    };
+
+
+    const DFS = (key)=>{
+        setWhichOut(true)
+        stopAddVtx(false)
+        setaddedge(false)
+
+        if (circleArray.length === 0) {
+            alert("Please Add Nodes First!!!")
+            return
+        }
+
+        let visited = new Array(circleArray.length).fill(false);
+        let result = []
+      
+
+        // Reject No need 
+        const delay = (ms) => new Promise((resolve, reject)=>{
+            setTimeout(() => {
+                resolve ()
+            }, ms);
+        });
+
+        // Set Heading of Algorithm
+        AddOutput.current.innerHTML += `<br><br><span style="font-size: large; font-weight: bold; margin: 12px;" > DFS  :  </span>`;
+
+        // Main DFS code
+        const DFSCode = async (node) =>{
+            visited[node] = true;
+            result.push(node)
+
+           for (let i = 0; i < circleArray.length; i++) {
+            if (parseInt(circleArray[i].id) == node) {
+                circleArray[i].color = "green"
+                setCircleArray([...circleArray])
+                DrawCanvas()
+                await delay(1000)
+                
+
+                // Adding Output in Operation Box
+                if (AddOutput.current) {
+                    AddOutput.current.innerHTML += `<span style="font-weight: bold; font-size: large; margin: 12px;">${node}</span>`;
+                }
+
+                circleArray[i].color = "black"
+                setCircleArray([...circleArray])
+                DrawCanvas()
+                break;
+            }
+            
+           }
+
+            for (const element of (Adjlist[node] || [])) {
+                if(!visited[element]) {
+                    await DFSCode(element)
+                }
+            }
+        }
+
+        DFSCode(parseInt(key))
+        console.log(result)
+        setWhichOut(false)
+    }
+
     return (
         <div className="container">
 
@@ -144,10 +235,10 @@ export default function ProjectPage() {
                 <div className="top-panel">
                     <h1>Operations</h1>
                     <h1>
-                        Mode: <span className="mode-name">Pointer</span>
+                        Mode: <span className="mode-name">{  addedge ? "Add Edge" : startAddVtx ? "Add Vertex" : "Pointer"}</span>
                     </h1>
                 </div>
-                <div className="operations"></div>
+                <div ref={AddOutput} className="operations"> {whichOut ? "DFS : " : ""}</div>
             </div>
 
             <div className="controls">
@@ -156,7 +247,7 @@ export default function ProjectPage() {
                 <div className="right">
                     <form>
                         <label><b>Starting Node</b></label>
-                        <input type="text" name="message" className="start-node" />
+                        <input type="number" className="start-node" onChange={(e)=>{setStartVal(e.target.value)}} value={startVal} />
                     </form>
                 </div>
 
@@ -164,7 +255,7 @@ export default function ProjectPage() {
                     <button onClick={Make_Vertex} className="vertexButton" data-clicked="false">{Vertix_data}</button>
                     <button onClick={Add_edge} className="edgeButton" data-clicked="false">{!addedge ? "Add Edge" : "Stop Edge"}</button>
                     <button className="bfsButton">BFS</button>
-                    <button className="dfsButton">DFS</button>
+                    <button onClick={()=> startVal === "" ? alert("Please Enter The Start key!!") : DFS(startVal)} className="dfsButton">DFS</button>
                     <button onClick={clearTheCanva} className="clearCanvas">Clear Canvas</button>
                 </div>
 

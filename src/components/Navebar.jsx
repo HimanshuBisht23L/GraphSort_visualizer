369474554c73f35f482e655091884f054446b6b9
HimanshuBisht23L <himanshiubisht4532@gmail.com>
2025-05-17 11:26:50 +0530

    navbar and responsive

--- src/components/Navebar.jsx
+++ src/components/Navebar.jsx
@@ -1,13 +1,16 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect} from 'react'
 import "../styles/Navebar.css"
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 
 export default function Navebar() {
 
+    // const [active, setactive] = useState("home");
     const [hamb, changehamb] = useState(false);
     const [popup, changepopup] = useState(false);
     const [scrolled, setScrolled] = useState(false);
     const navigate = useNavigate();
+    const location = useLocation();
+
 
     const Show_Nav_Links = () => {
         changehamb(!hamb);
@@ -28,7 +31,7 @@ export default function Navebar() {
             document.body.style.overflow = 'auto';
         }
     }, [hamb]);
-    
+
 
     useEffect(() => {
         const handleScroll = () => {
@@ -46,17 +49,17 @@ export default function Navebar() {
                 <ul className={hamb ? `mobile-ham ${popup ? 'Active' : ''}` : "nav-links"}>
                     <div className={hamb ? "heading-open" : "heading-close"}>
                         <h2>Cyber Ai</h2>
-                        <svg onClick={closeNavbar} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="28" height="28" color="#54af6b" fill="none">
+                        <svg onClick={closeNavbar} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="28" height="28" color="#005dff" fill="none">
                             <path d="M19.0005 4.99988L5.00049 18.9999M5.00049 4.99988L19.0005 18.9999"
-                                  stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" />
+                                stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" />
                         </svg>
                     </div>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/Test">Test</Link></li>
+                    <li><Link className={location.pathname === "/" ? "highlight" : ""} to="/">Home</Link></li>
+                    <li><Link className={location.pathname === "/Test" ? "highlight" : ""} to="/Test">Test</Link></li>
                 </ul>
 
                 <svg onClick={Show_Nav_Links} className='hamburger' xmlns="http://www.w3.org/2000/svg"
-                     viewBox="0 0 24 24" width="28" height="28" color="#54af6b" fill="none">
+                    viewBox="0 0 24 24" width="28" height="28" color="#54af6b" fill="none">
                     <path d="M4 5L20 5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                     <path d="M4 12L20 12" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                     <path d="M4 19L20 19" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
